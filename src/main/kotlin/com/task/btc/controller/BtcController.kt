ba0b1cd28c5856321b807a78d1957c651de814bf
leo <xpr35@live.com>
2020-12-12 16:41:11 +0700

    BTC-07: changes db requests

--- src/main/kotlin/com/task/btc/controller/BtcController.kt
+++ src/main/kotlin/com/task/btc/controller/BtcController.kt
@@ -2,18 +2,19 @@ package com.task.btc.controller
 
 import com.task.btc.dto.RecordDto
 import com.task.btc.dto.TimeDurationDto
-import com.task.btc.service.WalletService
+import com.task.btc.persistence.entity.RecordEntity
+import com.task.btc.service.WalletServiceV2
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class BtcController(
-        private val walletService: WalletService
+        private val walletService: WalletServiceV2,
 ) {
     @PostMapping("/saveRecord")
     fun saveRecord(@RequestBody record: RecordDto) =
             walletService.saveRecord(record)
 
     @GetMapping("/getRecord")
-    fun getRecord(@RequestBody duration: TimeDurationDto) =
+    fun getRecord(@RequestBody duration: TimeDurationDto) : List<RecordDto> =
             walletService.getRecord(duration)
 }
--- src/main/kotlin/com/task/btc/persistence/repository/RecordRepository.kt
+++ src/main/kotlin/com/task/btc/persistence/repository/RecordRepository.kt
@@ -2,14 +2,24 @@ package com.task.btc.persistence.repository
 
 import com.task.btc.persistence.entity.RecordEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.time.ZonedDateTime
 
 @Repository
-interface RecordRepository: JpaRepository<RecordEntity, Long> {
+interface RecordRepository : JpaRepository<RecordEntity, Long> {
     fun getByDatetimeAfterAndDatetimeBefore(
             after: ZonedDateTime, before: ZonedDateTime): List<RecordEntity>
 
     fun getByDatetime(dateTime: ZonedDateTime): RecordEntity?
-    fun getFirstByOrderByDatetimeDesc() : RecordEntity?
+    fun getFirstByOrderByDatetimeDesc(): RecordEntity?
+
+    @Query(REQ, nativeQuery = true)
+    fun getRecords(@Param("after") after: ZonedDateTime,
+                   @Param("before") before: ZonedDateTime): List<RecordEntity>
+
+    companion object {
+        private const val REQ = """SELECT amount "id", a.datetime, a.amount FROM (SELECT summed.datetime, sum(summed.amount) OVER (ORDER BY datetime) "amount" FROM (SELECT date_trunc('hour', record.datetime) "datetime", sum(record.amount) "amount" FROM record GROUP BY 1 ORDER BY 1) summed) a WHERE datetime BETWEEN :after AND :before"""
+    }
 }
